0262b7e6a2ae12d6b236836cea89b85d2e88b2a9
Juan Liut <liutjuan@gmail.com>
2023-07-14 18:42:24 +0200

    Initial Commit

---
+++ Galileo/GalileoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GalileoApp.swift
+//  Galileo
+//
+//  Created by Juan Liut on 14/07/23.
+//
+
+import SwiftUI
+
+@main
+struct GalileoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
